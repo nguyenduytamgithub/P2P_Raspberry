790e6cc2b6b03fa0ab7b74f51ca7f27b09ed4fd8
root <root@packer-debian-8-amd64.droplet.local>
2019-03-08 10:50:45 +0000

    update code 8_3

--- Node-red/run/merge.py
+++ Node-red/run/merge.py
@@ -7,14 +7,26 @@ def insertChar(mystring, position, chartoinsert ):
 	#longi = len(mystring)
 	mystring   =  mystring[:position] + chartoinsert + mystring[position:] 
 	return mystring  
-try:
-		f1=open("/root/.node-red/flows_SuperNode001.json", "rb")
-		f2=open("/root/.node-red/run/mqtt_broker.json", "rb")
+try:  
+		f0=open("/root/.node-red/run/id_broker.txt", "rb")
+		id_broker = f0.read().replace('\n','')  
+		with open("/root/.node-red/flows_SuperNode002.json", "rb") as json_main: 
+			json_data = json.load(json_main)
+			for item in json_data:
+				if item['id'] in ["elefos_mqtt_request"]:
+					item['broker'] = id_broker
+					print(item['broker'])
+				if item['id'] in ["elefos_mqtt_response"]:
+					item['broker'] = id_broker  
+		with open('/root/.node-red/flows_SuperNode002.json', 'w') as file:
+			json.dump(json_data, file, indent=2)
+		f1=open("/root/.node-red/flows_SuperNode002.json", "rb") 
+		f2=open("/root/.node-red/run/mqtt_broker.json", "rb") 
 		f3=open("/root/.node-red/run/flows_raspberrypi2.json","w")
 		string1 = f1.read().replace('\n','')
-		string2 = f2.read().replace('\n','')
-		length_str1 = len(string1)
-		string_merger = insertChar (string1,length_str1-1,string2)
+		string2 = f2.read().replace('\n','') 
+		length_str1 = len(string1) 
+		string_merger = insertChar(string1,length_str1-1,string2)		
 		f3.write(string_merger)
 		print(length_str1)
 		print(len(string_merger))
@@ -22,4 +34,5 @@ try:
 		
 except KeyboardInterrupt:
     pass
+
 
